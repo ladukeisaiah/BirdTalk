cba4266ef5651710f7aa59dfacf6a345bcab6345
Isaiah LaDuke <iladuke@atsapllc.com>
2024-03-07 18:02:15 -0500

    feat:Next image tag, style up

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,12 @@
+import { User, clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  return {id: user.id, name: user.username, profileImageUrl: user.imageUrl};
+};
 
 export const postRouter = createTRPCRouter({
   // hello: publicProcedure
@@ -36,7 +42,39 @@ export const postRouter = createTRPCRouter({
     });
   }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
-  })
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+    const users = (
+      await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+    ).map(filterUserForClient);
+
+    console.log(users);
+
+    return posts.map(post => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author || !author.name) 
+        // return {
+        //   post,
+        //   author: null,
+        // }
+        throw new TRPCError({ 
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+
+
+      return {
+        post,
+        author: {
+          ...author,
+          name: author.name,
+        },
+    }});
+  }),
 });
