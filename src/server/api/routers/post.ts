819c0be4b0ca5e9fa6bd185011f518fb605a4f31
Isaiah LaDuke <iladuke@atsapllc.com>
2024-03-08 23:49:00 -0500

    fix: fixing eslint errors that were crashing my build

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -80,7 +80,7 @@ export const postRouter = createTRPCRouter({
     return posts.map(post => {
       const author = users.find((user) => user.id === post.authorId);
 
-      if (!author || !author.username)
+      if (!author?.username)
         throw new TRPCError({ 
           code: "INTERNAL_SERVER_ERROR",
           message: "Author for post not found",
